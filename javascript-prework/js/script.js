667d42c5d4c1ad871c568b81d582084f8191ee91
Agnieszka Łupińska <agnieszka.gesiewska@o2.pl>
2025-01-01 14:19:33 +0100

    Ruch komputera

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -1,10 +1,25 @@
-let computerMove = `kamień`;
-let playerMove = 'papier'
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-printMessage('Zagrałem' + playerMove + 'Wygrywam')
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1) {
+  computerMove = 'kamień';
+}else if(randomNumber == 2) {
+    computerMove = 'papier'
+}else {
+    computerMove = 'nożyce'
+}
+
+printMessage('Mój ruch to: ' + computerMove);
